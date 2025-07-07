e469448f74ed131d1a005f5b28fe42917d8dc2ef
berylatieno <berylatieno30@gmail.com>
2025-07-07 19:38:59 +0300

    services init

---
+++ internal/services/authService.go
@@ -0,0 +1,21 @@
+package services
+
+func RegisterUserService() {
+	panic("not yet implemented")
+}
+
+func LoginService() {
+	panic("not yet implemented")
+}
+
+func LogoutService() {
+	panic("not yet implemented")
+}
+
+func ForgotPasswordService() {
+	panic("not yet implemented")
+}
+
+func ResetPasswordService() {
+	panic("not yet implemented")
+}
