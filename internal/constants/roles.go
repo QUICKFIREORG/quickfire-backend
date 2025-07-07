13c321aabda982731f5f6e54d2ed642e1b75e188
berylatieno <berylatieno30@gmail.com>
2025-07-07 20:44:07 +0300

    user role constants

---
+++ internal/constants/roles.go
@@ -0,0 +1,16 @@
+package constants
+
+const (
+	RoleUser  string = "user"
+	RoleAdmin string = "admin"
+	// RoleModerator string = "moderator"
+)
+
+func IsValidRole(role string) bool {
+	switch role {
+	case RoleUser, RoleAdmin:
+		return true
+	default:
+		return false
+	}
+}
