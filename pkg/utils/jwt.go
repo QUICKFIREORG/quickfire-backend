12669cc789dc4c3565f97d760f036d52fb26ef31
berylatieno <berylatieno30@gmail.com>
2025-07-07 21:23:39 +0300

    jwt

---
+++ pkg/utils/jwt.go
@@ -0,0 +1,54 @@
+// internal/utils/jwt.go
+package utils
+
+import (
+	"time"
+
+	"github.com/BerylCAtieno/quickfire-backend/config"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID uint   `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uint, role string) (string, error) {
+	expirationTime := time.Now().Add(time.Duration(config.Config.JwtExpirationHours) * time.Hour)
+	claims := &Claims{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "quickfire-app",
+			Subject:   "user_auth",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(config.Config.JwtSecret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ParseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.Config.JwtSecret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
+}
