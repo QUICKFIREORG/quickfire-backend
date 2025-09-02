77deffdcf2341cdc23b32a7000c4fbbeabda18e1
berylatieno <berylatieno30@gmail.com>
2025-09-02 10:28:32 +0300

    auth service files: on hold

---
+++ internal/repositories/user_repository.go
@@ -0,0 +1,10 @@
+package repositories
+
+// TODO: add repository methods
+// GetUserByEmail(email string) (*models.User, error)
+
+// CreateUser(user *models.User) error
+
+// GetUserByID(id uint) (*models.User, error)
+
+// (Optional) UpdateUser(user *models.User) error (for password reset)
